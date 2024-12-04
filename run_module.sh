59240bc2673940764684bbbc36098ed1a4dc5086
Damir Korencic <damir.korencic@gmail.com>
2024-12-04 17:25:19 +0100

    run_module.sh - handling of cmdline params for the py module

--- run_module.sh
+++ run_module.sh
@@ -3,8 +3,8 @@
 # it adds . to pythonpath, redirects the output to file, and runs the command
 
 # Check if a module path is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <module_path>"
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <module_path> <module_params>"
     exit 1
 fi
 
@@ -13,7 +13,15 @@ MODULE_PATH="$1"
 MODULE_NAME=$(basename "$MODULE_PATH" .py)
 OUTPUT_FILE="${MODULE_NAME}.out.txt"
 
+# take other parameters, if exist, and add as arguments to the module
+ARGS=""
+if [ "$#" -gt 1 ]; then
+    shift
+    ARGS="$@"
+    echo "Arguments: $ARGS"
+fi
+
 # run in the background
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
-nohup python "$MODULE_PATH" > "$OUTPUT_FILE" 2>&1 &
+nohup python "$MODULE_PATH" "$ARGS" > "$OUTPUT_FILE" 2>&1 &
 echo "Running $MODULE_PATH in background. Output redirected to $OUTPUT_FILE"
