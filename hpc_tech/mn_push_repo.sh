9cac7a56520e2119231d203272413f79053854a9
damirko <damir.korencic@gmail.com>
2024-12-23 13:48:06 +0100

    mn_push_repo.sh - for pushing local code to the remote machine, v1

--- hpc_tech/mn_push_repo.sh
+++ hpc_tech/mn_push_repo.sh
@@ -1,5 +1,39 @@
-# push local repo to marenostrum via ssh
-SSH_HOST="mna" # put full username@URL if no alias is configured in .ssh/config
-DEPLOY_FOLDER="~/code"
+#!/bin/bash
+# usage mn_push_repo [code folder] [deploy folder] [ssh host]
+# push local repo to a remote machine
+# code and deploy folders are PARENT folders of the project with code
+# which is assumed to be named 'gemma2-challenge'
+PROJECT="gemma2-challenge"
 
-# TODO finish
+CODE_FOLDER="." # where PROJECT is located locally
+# redefine CODE_FOLDER if passed as argument
+if [ $# -gt 0 ]; then
+    CODE_FOLDER=$1
+fi
+DEPLOY_FOLDER="~/code" # where PROJECT will be copied to remotely
+# redefine DEPLOY_FOLDER if passed as argument
+if [ $# -gt 1 ]; then
+    DEPLOY_FOLDER=$2
+fi
+SSH_HOST="mna" # put full username@URL if no alias is configured in .ssh/config
+# redefine SSH_HOST if passed as argument
+if [ $# -gt 2 ]; then
+    SSH_HOST=$3
+fi
+
+# delete remote project in the deploy folder, if exists
+echo 'Deleting remote project...'
+ssh $SSH_HOST "rm -rf $DEPLOY_FOLDER/$PROJECT"
+# copy local project to remote machine
+echo 'Copying local project to remote machine...'
+#scp -r $CODE_FOLDER/$PROJECT $SSH_HOST:$DEPLOY_FOLDER
+# do rsync instead of scp to avoid copying .git folder
+rsync -av --exclude '.git' $CODE_FOLDER/$PROJECT $SSH_HOST:$DEPLOY_FOLDER
+
+# create link-folders 'models', 'outputs', 'datasets' in the copied project
+ROOT_DIR="/gpfs/projects/ehpc124"
+# create links from $CODE_FOLDER/$PROJECT/x to $ROOT_DIR/x  where x is 'models', 'outputs', 'datasets'
+for x in models outputs datasets; do
+    echo "Creating link to $x..."
+    ssh $SSH_HOST "ln -s $ROOT_DIR/$x $DEPLOY_FOLDER/$PROJECT/$x"
+done
