59f0d0ff1fca13ac3157218f0ba961c5f6dc577c
Damir Korencic <damir.korencic@gmail.com>
2025-02-13 18:07:52 +0100

    code2cluster.sh - more general code copy script, to support leonardo also mt-train-2b-wiki-hr-deepspeed.yml - rolled back some hyperparams

---
+++ hpc_tech/code2cluster.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# usage mn_push_repo [ssh host] [code folder] [deploy folder]
+# push local repo to a remote machine
+# code and deploy folders are PARENT folders of the project with code
+# which is assumed to be named 'gemma2-challenge'
+PROJECT="gemma2-challenge"
+
+# SETUP VARIABLES
+
+# set variable to make a default SSH_HOST
+# put full username@URL if no alias is configured in .ssh/config
+SSH_HOST=""
+# redefine SSH_HOST if passed as argument
+if [ $# -gt 0 ]; then
+    SSH_HOST=$1
+fi
+
+CODE_FOLDER="." # where $PROJECT is located LOCALLY
+# redefine CODE_FOLDER if passed as argument
+if [ $# -gt 1 ]; then
+    CODE_FOLDER=$2
+fi
+
+DEPLOY_FOLDER="~/code" # where $PROJECT will be copied to remotely
+# redefine DEPLOY_FOLDER if passed as argument
+if [ $# -gt 2 ]; then
+    DEPLOY_FOLDER=$3
+fi
+
+# delete remote project in the deploy folder, if exists
+echo 'Deleting remote project...'
+ssh $SSH_HOST "rm -rf $DEPLOY_FOLDER/$PROJECT"
+
+echo 'Copying local project to remote machine...'
+# do copy; add files and folders to the exclude list as needed
+rsync -av --exclude={'**/.git','**/training_output'} $CODE_FOLDER/$PROJECT $SSH_HOST:$DEPLOY_FOLDER
+
+# create link-folders 'models', 'outputs', 'datasets' in the copied project
+ROOT_DIR="/gpfs/projects/ehpc124"
+# create links from $CODE_FOLDER/$PROJECT/x to $ROOT_DIR/x  where x is 'models', 'outputs', 'datasets'
+for x in models outputs datasets; do
+    echo "Creating link to $x..."
+    ssh $SSH_HOST "ln -s $ROOT_DIR/$x $DEPLOY_FOLDER/$PROJECT/$x"
+done
+
+echo 'initializing settings.py with a copy of settings.template.py...'
+ssh $SSH_HOST "rm $DEPLOY_FOLDER/$PROJECT/settings.py"
+ssh $SSH_HOST "cp $DEPLOY_FOLDER/$PROJECT/settings.template.py $DEPLOY_FOLDER/$PROJECT/settings.py"
