5fec50619aa22d7ec4e7628c0fd11f294c10d28a
damirko <damir.korencic@gmail.com>
2025-02-19 11:54:04 +0100

    code2cluster.sh - updgraded to enable copying code to a generic remote machine

--- hpc_tech/code2cluster.sh
+++ hpc_tech/code2cluster.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
-# usage mn_push_repo [ssh host] [code folder] [deploy folder]
+# usage code2cluster.sh [machine name] [code folder] [deploy folder]
 # push local repo to a remote machine
-# code and deploy folders are PARENT folders of the project with code
-# which is assumed to be named 'gemma2-challenge'
-PROJECT="gemma2-challenge"
+# machine name can be 'mn' or 'leo', make sure to define the
+#   correct SSH_HOST and REMOTE_ROOT for the machine below
+# code folder is the path to the local repo, the last folder
+#   in the path will be taken as the project name, and will be copied to remote machine
+# deploy folder is the path where the project will be copied to on the remote machine
+#   if not provided, it will be set to ~/code
 
-# SETUP VARIABLES
-
-# set variable to make a default SSH_HOST
-# put full username@URL if no alias is configured in .ssh/config
-SSH_HOST=""
-# redefine SSH_HOST if passed as argument
-if [ $# -gt 0 ]; then
-    SSH_HOST=$1
+## SETUP VARIABLES
+# set SSH_HOST based on the machine name
+if [ $# -lt 2 ]; then
+    echo "Machine name and code folder are required. Use 'mn' or 'leo' for machine name."
+    exit 1
 fi
 
-CODE_FOLDER="." # where $PROJECT is located LOCALLY
-# redefine CODE_FOLDER if passed as argument
-if [ $# -gt 1 ]; then
-    CODE_FOLDER=$2
+MACHINE_NAME=$1
+CODE_FOLDER=$2
+
+# depending on the machine name, set:
+# SSH_HOST - an alias in ~/.ssh/config or a full ssh address
+# REMOTE_ROOT - the 'root' shared project folder where 'models', 'outputs', 'datasets' are stored
+if [ "$MACHINE_NAME" == "mn" ]; then
+    SSH_HOST="mn"
+    REMOTE_ROOT="/gpfs/projects/ehpc124"
+elif [ "$MACHINE_NAME" == "leo" ]; then
+    SSH_HOST="leo" # placeholder for leo machine
+    REMOTE_ROOT="/leonardo_work/EUHPC_B18_060"
+else
+    echo "Invalid machine name. Use 'mn' or 'leo'."
+    exit 1
 fi
+
+# set PROJECT as the last folder in the path of the code folder
+PROJECT=$(basename "$CODE_FOLDER")
 
 DEPLOY_FOLDER="~/code" # where $PROJECT will be copied to remotely
 # redefine DEPLOY_FOLDER if passed as argument
@@ -33,14 +47,13 @@ ssh $SSH_HOST "rm -rf $DEPLOY_FOLDER/$PROJECT"
 
 echo 'Copying local project to remote machine...'
 # do copy; add files and folders to the exclude list as needed
-rsync -av --exclude={'**/.git','**/training_output'} $CODE_FOLDER/$PROJECT $SSH_HOST:$DEPLOY_FOLDER
+rsync -av --exclude={'**/.git','**/training_output'} $CODE_FOLDER $SSH_HOST:$DEPLOY_FOLDER
 
 # create link-folders 'models', 'outputs', 'datasets' in the copied project
-ROOT_DIR="/gpfs/projects/ehpc124"
-# create links from $CODE_FOLDER/$PROJECT/x to $ROOT_DIR/x  where x is 'models', 'outputs', 'datasets'
+# pointing to the shared folders in $REMOTE_ROOT with the same names
 for x in models outputs datasets; do
     echo "Creating link to $x..."
-    ssh $SSH_HOST "ln -s $ROOT_DIR/$x $DEPLOY_FOLDER/$PROJECT/$x"
+    ssh $SSH_HOST "ln -s $REMOTE_ROOT/$x $DEPLOY_FOLDER/$PROJECT/$x"
 done
 
 echo 'initializing settings.py with a copy of settings.template.py...'
