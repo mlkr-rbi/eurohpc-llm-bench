d35c146e38a2e1d7a795dbf034ee45aeafd1ac94
damirko <damir.korencic@gmail.com>
2025-03-11 17:36:29 +0100

    evaluation - max_predicted length calibration for parabstract (this is usecase specific, solve principally via params)

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -9,6 +9,7 @@ Sources:
 - GPU HF Inference: https://huggingface.co/docs/transformers/perf_infer_gpu_one
 
 """
+from pprint import pprint
 from typing import Dict, List
 from pathlib import Path
 import argparse
@@ -189,6 +190,7 @@ def get_datasets(datasets: List[str],
 
 def predict(**kwargs):
     """Get predictions for causal language model."""
+    dest_lang = kwargs['dest_lang']
     model = get_model(**kwargs)
     tokenizer = get_tokenizer(**kwargs)
     tokenizer.pad_token = tokenizer.eos_token
@@ -214,7 +216,7 @@ def predict(**kwargs):
         for i in tqdm(range(max_examples)): 
             # Tokenize the input prompt
             prompt = dataset['inputs'][i]
-            print(f"Prompt: {prompt}", "\n")
+            #print(f"Prompt: {prompt}", "\n")
             inputs = tokenizer(
                 prompt,
                 padding="max_length",
@@ -229,7 +231,13 @@ def predict(**kwargs):
             # Generate tokens using the model
             _kwargs = map_kwargs(GENERATE_KWARGS_MAP, **kwargs)
             del _kwargs['max_length'] # use 'max_new_tokens' instead of 'max_length' for precise control
-            _kwargs['max_new_tokens'] = int(real_length * 1.15) if real_length != 0 else max_length
+            # TODO: solve principally via params, this is a parabstract hack
+            if dest_lang == 'hr':
+                max_new_tokens = int(real_length * 1.5) if real_length != 0 else max_length
+            else:
+                max_new_tokens = int(real_length * 1.0) if real_length != 0 else max_length
+            #print(f"Dest: {dest_lang}, real length: {real_length}, Max new tokens: {max_new_tokens}", "\n")
+            _kwargs['max_new_tokens'] = max_new_tokens
             #print(f"Max new tokens: {_kwargs['max_new_tokens']}", "\n")
             outputs = model.generate(eos_token_id=tokenizer.eos_token_id,
                                      pad_token_id=tokenizer.pad_token_id,
