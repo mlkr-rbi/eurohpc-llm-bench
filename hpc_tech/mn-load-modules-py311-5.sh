a958c902c44244d8712565139fe641c74c301d31
Damir Korencic <damir.korencic@gmail.com>
2025-06-20 19:17:18 +0200

    mn-load-modules-py311-5.sh - openmpi setup added requirements.txt - enabled tf-keras by default, for nm

--- hpc_tech/mn-load-modules-py311-5.sh
+++ hpc_tech/mn-load-modules-py311-5.sh
@@ -1,1 +1,5 @@
+# alternative, use openmpi instead of impi
+#module load bsc mkl intel hdf5 sqlite3/3.45.2-gcc cuda/12.6 python/3.11.5-gcc intel/2024.0.1-sycl_cuda openmpi/4.1.6
+
+# version last used in march 2025
 module load bsc mkl intel impi hdf5 sqlite3/3.45.2-gcc cuda/12.6 python/3.11.5-gcc
